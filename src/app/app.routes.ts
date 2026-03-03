3775b18c7329c69316fdb16d3e9cc176dde23a1e
user <mahamatnourbrahim235@gmail.com>
2026-03-03 16:51:30 +0000

    integration et routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { FormDemandeComponent } from './features/demande-rv/form-demande/form-demande.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dash',
+        component: DashboardComponent
+    },
+    {
+        path: 'form-demande',
+        component: FormDemandeComponent
+    },
+    {
+        path: '',
+        redirectTo: 'dash',
+        pathMatch: 'full'
+    }
+
+];
