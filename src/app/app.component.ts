61cc4ceb9e7b3be2358db5bbfd545abb93c30928
user <mahamatnourbrahim235@gmail.com>
2026-03-16 15:01:29 +0000

    Creation d'un observable avec abonnement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,75 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from '@layout/private/header/header.component';
+import { Observable, Subscriber, Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy,OnInit {
   title = 'gestionRdv-NourBrahim';
+  letter : string = '';
+  letter1 : string = '';
+  letter2 : string = '';
+  subscription1? : Subscription;
+  subscription2? : Subscription;
+  subscription3? : Subscription;
+  helloObservable? : Observable<string>;
+
+  constructor() {
+    this.helloObservable = new Observable<string>((subscriber: Subscriber<string>) => {
+      const message = 'Hello, Angular';
+      for(let i=0; i<message.length; i++) {
+        setTimeout(() => {
+          subscriber.next(message[i]);
+      }, 1000 * (i+1));
+      }
+      setInterval(() => {
+        subscriber.complete();  
+      }, 1000 * (message.length + 1));
+    });
+  }
+
+  ngOnInit(): void {
+
+    const helloObserver1$ ={
+      next: (letter: string) => {
+        this.letter += letter;
+      },
+      complete: () => {
+        console.log('Observable 1 completed');
+      }
+    };
+
+    const helloObserver2$ ={
+      next: (letter: string) => {
+        this.letter += letter;
+      },
+      complete: () => {
+        console.log('Observable 2 completed');
+      }
+    };
+
+    this.subscription1 = this.helloObservable?.subscribe(helloObserver1$);
+    this.subscription2 = this.helloObservable?.subscribe(helloObserver2$);
+    this.subscription3 = this.helloObservable?.subscribe({
+      next: (letter: string) => {
+        this.letter += letter;
+      },
+      complete: () => {
+        console.log('Observable 3 completed');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription1?.unsubscribe();
+    this.subscription2?.unsubscribe();
+    this.subscription3?.unsubscribe();
+    console.log('All subscriptions unsubscribed');
+  }
 }
--- src/app/core/models/patient.model.ts
+++ src/app/core/models/patient.model.ts
@@ -7,5 +7,9 @@ export interface PatientModel{
     adresse: string;
     antecedents: string;
 }
+//Omit creer un type qui contient tous les champs de PatientModel sauf id, car id est généré automatiquement lors de la création d'un patient.
+//Pick creer un type qui contient seulement les champs nom, prenom, telephone, adresse et antecedents, car ce sont les champs nécessaires pour créer un patient.
+//Required creer un type qui rend tous les champs de PatientModel obligatoires, car lors de la création d'un patient, tous les champs sont nécessaires.
+//Partial creer un type qui rend tous les champs de PatientModel optionnels, car lors de la mise à jour d'un patient, on peut ne mettre à jour que certains champs.
 
 export type PatientRequest = Omit<PatientModel, 'id'>;
